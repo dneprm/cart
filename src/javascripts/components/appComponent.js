b1e7c4d57ca2f0596cda0f19d9871a5b9ed23e4e
dneprm <turkomariya@gmail.com>
2015-03-31 13:10:06 +0000

    A little change in AppComponent.js

--- src/javascripts/components/appComponent.js
+++ src/javascripts/components/appComponent.js
@@ -6,15 +6,12 @@ var CartStore = require('../store/CartStore');
 
 
 var AppComponent = React.createClass({
-  mixins: [Reflux.listenTo(CartStore, "onUpdateState")],
+  mixins: [Reflux.connect(CartStore, 'cart')],
+  
   getInitialState: function() {
     return { cart: [] };
   },
-  onUpdateState: function(cart) {
-    this.setState({cart: cart});
-        console.log(cart);
-
-  },
+  
   render: function() {
     return (
       <div className="app">
